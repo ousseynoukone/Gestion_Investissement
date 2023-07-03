c5fabe0e25b9a05204dd56f540a2e7c5b0a29529
ousseynoukone <ousseynou781227@gmail.com>
2023-07-03 23:44:59 +0000

    last update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,139 +2,39 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="template/authe1.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<div class="container">
     <link rel="stylesheet" href="template/fontawesome-free-5.15.3-web/css/all.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div id="container" class="container">
-		{{-- <nav class="nav">
-			<a class="nav-logo" href="#">
-			<img src="template/logo.png" alt="" style="width: 70px;">
-			   </a>
-				<div class="nav-menu" id="navMenu">
-					<ul>
-						<li><a href="#" class="link active"></a></li>
-						<li><a href="#" class="link"></a></li>
-						<li><a href="#" class="link"></a></li>
-						<li><a href="#" class="link"></a></li>
-					</ul>
-				</div>
-                <div class="nav-button">
-            <button class="btn white-btn" id="loginBtn" onclick="login()"><i class="fa fa-home"></i></i><a href="/" style="color: black;"></i>Accueil</h2></a></button>
-            
+        <link rel="stylesheet" href="template/index.css">
+
+        <div class="bg-primary  rounded" style="background-color: red;width:9rem; padding:0.4rem;border-radius:10px; display:inline-block; position:absolute; z-index:1; margin: 1rem;    " >
+            <a href="/" class="" style="text-decoration:none; color:rgb(255, 255, 255);">
+                <h2 class="text-primary"><i class="fa fa-user-edit me-2"></i>Accueil</h2>
+            </a>	
         </div>
 
-				<div class="nav-menu-btn">
-					<i class="bx bx-menu" onclick="myMenuFunction()"></i>
-				</div>
-			</nav> --}}
-		<!-- FORM SECTION -->
-        <div class="nav-button">
-            <button class="btn white-btn" id="loginBtn" onclick="login()"><i class="fa fa-home"></i></i><a href="/" style="color: black;"></i>Accueil</h2></a></button>
+        <div class="container" style="background-color: rgb(2, 1, 7) !important;">
+
+	<div class="screen">
+
+		<div class="screen__content">
+
+            @if(Auth::user()==null)
             
-        </div>
-		<div class="row">
-    
-			<!-- SIGN UP -->
-			
-			<div class="col align-items-center flex-col sign-up" >
-				<form class="login" method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
-					@csrf
-				
-				<div class="form-wrapper align-items-center">
-					<div class="form sign-up">
-						<!-- Name -->
-						<div class="input-group">
-							<i class="login__icon fas fa-user"></i>
-					       <input type="text" class="login__input" placeholder="Nom complet" id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name"/>
-                           <x-input-error :messages="$errors->get('name')" class="mt-2" />
-						</div>
-						 <!-- Email Address -->
-						<div class="input-group">
-							<i class="login__icon fas fa-envelope"></i>
-							<input type="Email" class="login__input" placeholder="Email" id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username"/>
-                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-						</div>
-						<!-- select role -->
-						<div class="input-group">
-							
-					<!-- <x-input-label for="role" :value="__('Je suis :  ')" /> -->
-							<select id="role" onchange="voitureEnable()" name="role" class="login__input block mt-1 w-full custom-select" style="height: 40px; width: 400px;"  required>
-								<option value="">Qui Etes Vous.........</option>
-								<option value="entrepreneur" {{ old('role') == 'entrepreneurs' ? 'selected' : '' }}>Entrepreneur</option>
-								<option value="investisseur" {{ old('role') == 'investisseurs' ? 'selected' : '' }}>Investisseur</option>
-							</select>
-					        <x-input-error :messages="$errors->get('role')" class="mt-2" />
-						  </div>
-						  <!-- image -->
-						  <div class="input-group bubble">
-							<input type="file" class="login__input" placeholder="	" id="photo" class="block mt-1 w-full"
-								name="photo" accept="image/*" />
-							<x-input-error :messages="$errors->get('photo')" class="mt-2" />
-						  </div>
-						  
-						  
-						   <!-- Password -->
-						
-						<div class="input-group">
-							<i class="login__icon fas fa-lock"></i>
-                           <input type="password" class="login__input" placeholder="Mots de passe" id="password" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password"
-                                    required autocomplete="new-password" />
-									<x-input-error :messages="$errors->get('password')" class="mt-2" />
-						</div>
-						<!-- Confirm Password -->
-						<div class="input-group ">
-							<i class="login__icon fas fa-lock"></i>
-                                      <input type="password" class="login__input" placeholder="Confirmez mots de passe" id="password_confirmation" class="block mt-1 w-full"
-                                        type="password"
-                                        name="password_confirmation" required autocomplete="new-password" />
-                           <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-						</div>
-						<button  >
-							<span class="button__text">Inscription</span>
-							<i class="button__icon fas fa-chevron-right"></i>
-							<x-primary-button class="ml-4 ">
-								{{ __('') }}
-							</x-primary-button>
-						</button>
-						<p>
-							<span>
-								Vous avez déjà un compte?
-							</span>
-							<b onclick="toggle()" class="pointer">
-								Connexion
-							</b>
-						</p>
-					</div>
-				</div>
-				</form>
-			
-			</div>
-			
-		
-		
-			<!-- END SIGN UP -->
-			<!-- SIGN IN -->
-			<div class="col align-items-center flex-col sign-in">
-				@if(Auth::user()==null)
-            
-				<form method="POST" action="{{ route('login') }}" class="login">
-				  @csrf
-				<div class="form-wrapper align-items-center">
-					<div class="form sign-in">
-						<div class="input-group">
-							<i class="login__icon fas fa-envelope"></i>
-					     <input  for="email" type="text" class="login__input" placeholder="Email" id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" >
+			<form method="POST" action="{{ route('login') }}" class="login">
+            @csrf
+
+                    <!-- Email Address -->
+                    <div class="login__field">
+                    <i class="login__icon fas fa-envelope"></i>
+					<input  for="email" type="text" class="login__input" placeholder="Email" id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" >
                         <!-- <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" /> -->
                         <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
 
-						</div>
-						<div class="input-group">
-							<i class="login__icon fas fa-lock"></i>
+                    <!-- Password -->
+                    <div  class="login__field">
+                    <i class="login__icon fas fa-lock"></i>
 					<input type="password" class="login__input" placeholder="Mots de passe" for="password" :value="__('Password')" id="password" class="block mt-1 w-full"
                                         type="password"
                                         name="password"
@@ -143,92 +43,59 @@
                         
 
                         <x-input-error :messages="$errors->get('password')" class="mt-2" />
-						</div>
-						<button class="button login__submit" class="flex items-center justify-end mt-4">
-							<span class="button__text">Connexion</span>
-							<i class="button__icon fas fa-chevron-right"></i>
-							<x-primary-button class="ml-3">
-									{{ __('') }}
-								</x-primary-button>
-						 </button>
-						<p>
-							<b>
-								Mot de passe oublié?
-							</b>
-						</p>
-						<p>
-							<span>
-								Vous n'avez pas de compte ?
-							</span>
-							<b onclick="toggle()" class="pointer">
-								Inscrivez-vous ici
-							</b>
-						</p>
-					</div>
-				</div>
-				<div class="form-wrapper">
-		
-				</div>
+                    </div>
+
+
+                    <button class="button login__submit" class="flex items-center justify-end mt-4">
+                        <span class="button__text">Connexion</span>
+                        <i class="button__icon fas fa-chevron-right"></i>
+                        <x-primary-button class="ml-3">
+                                {{ __('') }}
+                            </x-primary-button>
+				     </button>
+            	
 			</form>
             @else
             <a href="{{ Auth::user()->role=="entrepreneur" ? route('entrepreneurs.index') : route('investisseurs.index')  }}" style="text-decoration: none !important;color:aliceblue ; " >Accueil</a>
 
 
             @endif
-			</div>
-			<!-- END SIGN IN -->
-		</div>
+           
+			<div class="social-login">
+				<h4>Compte Inexistant ?</h4>
+                <div>
+                    @if (Route::has('login'))
+                            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                                @auth
+                                    <a href="{{ Auth::user()->role=="entrepreneur" ? route('entrepreneurs.index') : route('investisseurs.index')  }}" style="text-decoration: none !important;color:aliceblue ; " >Accueil</a>
+                                @else
+                                   
 
-		<!-- END FORM SECTION -->
-		<!-- CONTENT SECTION -->
-		<div class="row content-row">
-            
-			<!-- SIGN IN CONTENT -->
-			<div class="col align-items-center flex-col">
-				<div class="text sign-in">
-					<h2 >
-						Bienvenue
-					</h2>
-	
-				</div>
-				<div class="img sign-in">
-		
+                                    @if (Route::has('register'))
+                                        <a href="{{ route('register') }}" style="color:aliceblue" >Inscription</a>
+                                    @endif
+                                @endauth
+                            </div>
+                    @endif
+                </div>
+                <div class="social-icons">
+					<a href="#" class="social-login__icon fab fa-instagram"></a>
+					<a href="#" class="social-login__icon fab fa-facebook"></a>
+					<a href="#" class="social-login__icon fab fa-twitter"></a>
 				</div>
 			</div>
-			<!-- END SIGN IN CONTENT -->
-			<!-- SIGN UP CONTENT -->
-			<div class="col align-items-center flex-col">
-				<div class="img sign-up">
-				
-				</div>
-				<div class="text sign-up">
-					<h2>
-						Rejoignez-nous
-					</h2>
-	
-				</div>
-			</div>
-			<!-- END SIGN UP CONTENT -->
 		</div>
-		<!-- END CONTENT SECTION -->
+		<div class="screen__background">
+			<span class="screen__background__shape screen__background__shape4"></span>
+			<span class="screen__background__shape screen__background__shape3"></span>		
+			<span class="screen__background__shape screen__background__shape2"></span>
+			<span class="screen__background__shape screen__background__shape1"></span>
+		</div>		
 	</div>
-    <script>
-        
- let container = document.getElementById('container')
-
-toggle = () => {
-	container.classList.toggle('sign-in')
-	container.classList.toggle('sign-up')
-}
-
-setTimeout(() => {
-	container.classList.add('sign-in')
-}, 200)
-    </script>
-
-
-
-
+</div>
+    <title>Document</title>
+</head>
+<body>
     
 </body>
 </html>
